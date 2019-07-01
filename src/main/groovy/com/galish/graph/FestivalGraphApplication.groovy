b89f8e11425af8f7dbf7d56530be2ea49ab1e163
galishtech <galishtech@gmail.com>
2019-07-01 20:39:12 +0100

    Update FestivalGraphApplication.groovy

--- src/main/groovy/com/galish/graph/FestivalGraphApplication.groovy
+++ src/main/groovy/com/galish/graph/FestivalGraphApplication.groovy
@@ -2,7 +2,7 @@ package com.galish.graph
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-
+//sdfsdf
 @SpringBootApplication
 class FestivalGraphApplication {
 
